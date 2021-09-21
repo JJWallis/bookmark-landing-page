6831fbb916310ae8fc4fd17fd754483663bb4a84
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-09-21 19:03:50 +0100

    Mobile nav ip

--- src/scripts/hidden.js
+++ src/scripts/hidden.js
@@ -1,1 +1,21 @@
-// Make sure mobile nav not read out by screen readers at wide
+const header = document.querySelector('.hdr_cnt')
+const mobileNav = document.querySelector('.hdr_mobile-nav-ct')
+const classList = (el, act, val) => el.classList[act](val)
+const viewport = () => window.innerWidth
+
+header.addEventListener('click', e => {
+   if (viewport() < 800) {
+      if (e.target.matches('.hamburger-btn') || e.target.matches('.btn-hmb')) {
+         classList(mobileNav, 'remove', 'hidden')
+      } else if (
+         e.target.matches('.close-btn') ||
+         e.target.matches('.icn-close')
+      ) {
+         classList(mobileNav, 'add', 'hidden')
+      }
+   }
+})
+
+// Opactity for hamburger + logo vs hidden class
+// Match viewport size to CSS breakpoints
+// Modules to export
